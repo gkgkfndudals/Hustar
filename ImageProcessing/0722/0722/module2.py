8e6db90ce169b787d05ed1d44e926af1d2a2154f
gkgkfndudals <67456405+gkgkfndudals@users.noreply.github.com>
2020-08-12 20:54:01 +0900

    Python operation, numpy

---
+++ ImageProcessing/0722/0722/module2.py
@@ -0,0 +1,13 @@
+
+a=2
+print("a: " ,a)
+a+=3
+print("a+=3. a: " , a);
+a-=1
+print("a-=1 , a:" ,a);
+
+a//=2
+print("a//=2 . a:", a);
+
+a**=3
+print("a**=3. a: " ,a);
---
+++ ImageProcessing/0722/0722/module3.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+data1 = [1,2,3,4,5]
+
+np1_arr = np.array(data1)
+
+print(np1_arr)
